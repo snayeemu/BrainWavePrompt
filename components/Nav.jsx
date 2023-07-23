adb83bb3901f5c1e125abbeb2ea3b3960715ac0f
snayeemu <snayeemu2@gmail.com>
2023-07-24 03:11:04 +0600

    24

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,9 +2,12 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { signOut } from "next-auth/react";
 import React from "react";
 
 const Nav = () => {
+  const isUserLogged = true;
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href={"/"} className="flex gap-2 flex-center">
@@ -15,10 +18,26 @@ const Nav = () => {
           height={40}
           className="object-contain"
         />
+        <p className="logo_text">BrainWavePrompt</p>
       </Link>
 
-      <p className="logo_text">BrainWavePrompt</p>
-      {/* 01:03 */}
+      {/* Desktop Navigation */}
+      <div className="hidden sm:flex">
+        {isUserLogged ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href={"create-prompt"} className="black_btn">
+              Create Post
+            </Link>
+
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+            {/* 01:06 */}
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </nav>
   );
 };
