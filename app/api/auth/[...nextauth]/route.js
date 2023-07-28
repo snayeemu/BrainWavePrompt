6406a6079ab035118e63b5fc3c4103fc82ec6c5b
snayeemu <snayeemu2@gmail.com>
2023-07-28 12:04:30 +0600

    28-07

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { signIn } from "next-auth/react";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: "",
+      clientSecret: "",
+    }),
+  ],
+  async session({ session }) {},
+  async signIn({ profile }) {},
+});
+
+export { handler as GET, handler as POST };
+// 1:20
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import Nav from "@components/Nav";
 import "@styles/global.css";
+import Provider from "@components/Provider";
 
 export const metadata = {
   title: "BrainwavePrompt",
@@ -10,14 +11,16 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
 
-        <main className="app">
-          <Nav />
-          {children}
-        </main>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
